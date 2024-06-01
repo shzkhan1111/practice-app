d906e0010c7c41a027dcbf541be8218ce71dc08e
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2024-06-01 16:17:52 +0500

    Form Array Dynamic reactive form example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice-app';
+  nestedForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.nestedForm = this.fb.group({
+      categories: this.fb.array([
+        this.fb.group({
+          categoryName: this.fb.control('', Validators.required),
+          items: this.fb.array([
+            this.fb.control('', Validators.required)
+          ])
+        })
+      ])
+    });
+  }
+
+  get categories() {
+    return this.nestedForm.get('categories') as FormArray;
+  }
+
+  getItems(categoryIndex: number) {
+    return this.categories.at(categoryIndex).get('items') as FormArray;
+  }
+  addCategory() {
+    this.categories.push(this.fb.group({
+      categoryName: this.fb.control('', Validators.required),
+      items: this.fb.array([
+        this.fb.control('', Validators.required)
+      ])
+    }));
+  }
+  addItem(categoryIndex: number) {
+    this.getItems(categoryIndex).push(this.fb.control('', Validators.required));
+  }
+
+  removeCategory(index: number) {
+    this.categories.removeAt(index);
+  }
+  removeItem(categoryIndex: number, itemIndex: number) {
+    this.getItems(categoryIndex).removeAt(itemIndex);
+  }
+
+  onSubmit() {
+    console.log(this.nestedForm.value);
+  }
+
 }
